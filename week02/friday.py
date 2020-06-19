66e39aa2ccde4ba4c7078307cfdb03b185bed87b
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-06-19 11:20:49 -0500

    update complete ticket message

--- week02/friday.py
+++ week02/friday.py
@@ -13,3 +13,35 @@ message = "Thanks for shopping with us today!"
 
 # create a top border
 print("*" * 50)
+
+# print company information first, using format
+print(f"\t\t{company_name.title()}")
+print(f"\t\t{company_address}")
+print(f"\t\t{company_city}")
+
+# print a line between sections
+print("=" * 50)
+
+# print out header for section of items
+print("\tProduct Name \tProduct Price")
+
+# create a print statement for each product
+print(f"\t{p1_name.title()}\t\t${p1_price}")
+print(f"\t{p2_name.title()}\t\t${p2_price}")
+print(f"\t{p3_name.title()}\t\t${p3_price}")
+
+# print a line between sections
+print("=" * 50)
+
+# print out header for sections of total
+print("\t\t\tTotal")
+
+# calculate total price and print out
+total = p1_price + p2_price + p3_price
+print(f"\t\t${total}")
+
+# print a line between sections
+print("=" * 50)
+
+# output a thank you message
+print(f"\n\t{message}\n")
