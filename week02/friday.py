236532537db8dbf20b41c288247b5f72701331d6
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-06-19 10:58:08 -0500

    init friday exercise

---
+++ week02/friday.py
@@ -0,0 +1,15 @@
+# create a product and price for three items
+p1_name, p1_price = "Books", 49.99
+p2_name, p2_price = "Computer", 579.99
+p3_name, p3_price = "Monitor", 124.89
+
+# create a company name and information
+company_name = "coding temple, inc."
+company_address = "283 Franklin St."
+company_city = "Boston, MA"
+
+# declare ending message
+message = "Thanks for shopping with us today!"
+
+# create a top border
+print("*" * 50)
