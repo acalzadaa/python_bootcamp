0ae959b8492f58fceac89dbe72f482de7f6096ef
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-08 16:12:50 -0500

    add return function lesson

---
+++ week05/wednesday.py
@@ -0,0 +1,36 @@
+# Return Statement
+
+def addNums(num1, num2):
+    return num1 + num2
+
+num = addNums(3, 4)
+print(num)
+
+# Ternary Operator
+# shorthand syntax using a ternary operator
+
+def searchList(aList, el):
+    return True if el in aList else False
+
+result = searchList(["one", 2, "three"], 3)
+print(f"deberia ser falso: {result}")
+
+result = searchList(["one", 2, "three"], 2)
+print(f"deberia ser verdadero: {result}")
+
+# Full Name: Create a function that takes in a first and last name 
+# and returns the two names joined together.
+
+def joinNames(firstName, lastName):
+    return firstName + " " + lastName
+
+print(joinNames("Alex", "Calzada"))
+
+# User Input: Within a function, ask for user input. 
+# Have this function return that input to be stored in a variable outside of the function. 
+# Then print out the input.
+
+def returnTheInput():
+    return input("Write a word!: ")
+
+print(returnTheInput())
