bbddd97b5566b83311089341cb7f2b90d906a8bf
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-10 12:58:45 -0500

    update functions names to comply with soniclint

--- week05/wednesday.py
+++ week05/wednesday.py
@@ -1,37 +1,37 @@
 # Return Statement
 
-def addNums(num1, num2):
+def addnums(num1, num2):
     return num1 + num2
 
-num = addNums(3, 4)
+num = addnums(3, 4)
 print(num)
 
 # Ternary Operator
 # shorthand syntax using a ternary operator
 
-def searchList(aList, el):
-    return True if el in aList else False
+def searchlist(alist, el):
+    return True if el in alist else False
 
-result = searchList(["one", 2, "three"], 3)
+result = searchlist(["one", 2, "three"], 3)
 print(f"deberia ser falso: {result}")
 
-result = searchList(["one", 2, "three"], 2)
+result = searchlist(["one", 2, "three"], 2)
 print(f"deberia ser verdadero: {result}")
 
 # Full Name: Create a function that takes in a first and last name 
 # and returns the two names joined together.
 
-def joinNames(firstName, lastName):
-    return firstName + " " + lastName
+def joinnames(firstname, lastname):
+    return firstname + " " + lastname
 
-print(joinNames("Alex", "Calzada"))
+print(joinnames("Alex", "Calzada"))
 
 # User Input: Within a function, ask for user input. 
 # Have this function return that input to be stored in a variable outside of the function. 
 # Then print out the input.
 
-def returnTheInput():
+def returntheinput():
     return input("Write a word!: ")
 
-print(returnTheInput())
+print(returntheinput())
 
