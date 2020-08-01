c9cc4cb4f0be16c99a35bc74146ff77a71a98486
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-07-31 19:10:16 -0500

    add binarysearch with recursion

--- week08/friday.py
+++ week08/friday.py
@@ -10,23 +10,14 @@ def binarySearch(aList, num):
     #step 1 - sort the list
     aList.sort()
 
-    # step 6: setup a loop to repeat steps 2 through 6 until list is empty
-    while aList:
-        #step 2 - find the middle index
-        mid = len(aList) // 2
-        
-        #step 3 - check the value at middle index, if it's equal to num return True
-        if aList[mid] == num:
-            return True
-
-        #step 4 - check if value is greater, cut off right half of list
-        elif aList[mid]> num:
-            aList = aList[:mid]
-
-        #step 5 - check if value is lesser, cut of left half of list
-        elif aList[mid]< num:
-            aList = aList[mid+1:]
-    return False
+    if(not aList):
+        return False
+    elif aList[len(aList) // 2] == num:
+        return True
+    elif aList[len(aList) // 2] > num:
+        return binarySearch(aList[:len(aList) // 2], num)
+    elif aList[len(aList) // 2] < num:
+        return binarySearch(aList[(len(aList) // 2)+1:], num)
 
 print(sorted(nums))
 print(binarySearch(nums, 3))
